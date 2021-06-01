31ec0a330a9c320a6f3d7d6284d4efca768d8af4
GaneshDesk <koradeganesh.r@gmail.com>
2021-06-01 11:55:10 +0530

    String Programs

---
+++ String2.f95
@@ -0,0 +1,14 @@
+! ASCI to char
+
+program testingChars
+implicit none
+
+   character:: ch
+   integer:: i
+   
+   do i = 65, 90
+      ch = achar(i)
+      print*, i, ' ', ch
+   end do
+   
+end program testingChars
